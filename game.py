1fde24f312f1a13a5f24724f52df8fc879ea47b1
AlgoClassWork <kyxec.play@gmail.com>
2025-06-05 14:14:21 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 from pygame import * 
 
-class GameSprite():
+class GameSprite(sprite.Sprite):
     def __init__(self, img, width, height, speed, x, y):
+        super().__init__()
         self.image = transform.scale( image.load(img), (width, height) )
         self.rect = self.image.get_rect()
         self.speed = speed
@@ -16,24 +17,44 @@ class Player(GameSprite):
         mouse_x, y = mouse.get_pos()
         self.rect.centerx = mouse_x
 
+    def fire(self):
+        #laser_sound.play()
+        laser = Laser(img='laser.png', width=5, height=100, speed=10,
+                               x=player.rect.centerx, y=player.rect.y)
+        lasers.add(laser)
+
+class Laser(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed 
+
+
 player = Player(img='player.png',width=100, height=100, speed=5, x=350, y=400)
+
+lasers = sprite.Group()
 
 window = display.set_mode( (800,500) )
 display.set_caption('Шутер')
 background = transform.scale( image.load('background.png'), (800,500) )
 clock = time.Clock()
 
+#mixer.init()
+#mixer.music.load('music.mp3')
+#mixer.music.play()
+#laser_sound = mixer.Sound('laser.mp3')
+
 while True:
     for some_event in event.get():
         if some_event.type == QUIT:
             exit()
         elif some_event.type == MOUSEBUTTONDOWN and some_event.button == BUTTON_LEFT:
-            print('выстрел')
-
+            player.fire()
+            
     window.blit(background, (0, 0))
     player.show()
+    lasers.draw(window)
 
     player.move()
+    lasers.update()
     
     display.update()
     clock.tick(100)
